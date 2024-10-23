f5ab99512af5860c93c1589169af55f48460b29f
shmakov.a <shmakov.a@skbkontur.ru>
2024-10-23 07:38:03 +0300

    mvc_proxy_based подход

--- RazorPage/mvc-client/Controllers/HomeController.cs
+++ RazorPage/mvc-client/Controllers/HomeController.cs
@@ -1,14 +1,22 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Authentication;
+﻿using System;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using mvc_client.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 
 namespace mvc_client.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -23,11 +31,34 @@ namespace mvc_client.Controllers
         [Authorize]
         public IActionResult Logout()
         {
-            var prop = new AuthenticationProperties
-            {
-                RedirectUri = "/Home"
-            };
-            return new SignOutResult(new [] {"Cookies", "oidc"}, prop);
+            HttpContext.Request.Cookies.TryGetValue("IdToken", out var idToken);
+
+            // 1. удаляем все куки (это умеет делать сам envoy, но тут проще)
+            Response.Cookies.Append("IdToken", string.Empty, new CookieOptions
+            { 
+                Expires = DateTime.Now.AddDays(-1)
+            });
+            Response.Cookies.Append("AccessToken", string.Empty, new CookieOptions
+            { 
+                Expires = DateTime.Now.AddDays(-1)
+            });
+            Response.Cookies.Append("RefreshToken", string.Empty, new CookieOptions
+            { 
+                Expires = DateTime.Now.AddDays(-1) 
+            });
+            
+            Response.Cookies.Append("OauthExpires", string.Empty, new CookieOptions
+            { 
+                Expires = DateTime.Now.AddDays(-1) 
+            });
+            
+            Response.Cookies.Append("OauthHMAC", string.Empty, new CookieOptions
+            { 
+                Expires = DateTime.Now.AddDays(-1)
+            });
+
+            return Redirect($"https://idp.ca.testkontur.ru:8444/realms/Kontur/protocol/openid-connect/logout" +
+                                  $"?id_token_hint={idToken}&post_logout_redirect_uri=https://localhost:14100");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- RazorPage/mvc-client/Startup.cs
+++ RazorPage/mvc-client/Startup.cs
@@ -1,16 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using Microsoft.IdentityModel.Tokens;
 
 namespace mvc_client
@@ -28,35 +21,21 @@ namespace mvc_client
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            
+
             services.AddAuthentication(options =>
             {
-                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = "oidc";
-            })
-            .AddCookie(options =>
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
             {
-                options.Cookie.Name = "NetwealthWebsite";
-            })
-            .AddOpenIdConnect("oidc", options =>
+                options.Authority = "https://idp.ca.testkontur.ru:8444/realms/Kontur";
+                  
+                options.TokenValidationParameters = new TokenValidationParameters
                 {
-                    options.Authority = "https://idp.ca.testkontur.ru:8444/realms/Kontur";
-                    options.ClientId = "TestShmakov";
-                    options.ClientSecret = Environment.GetEnvironmentVariable("FEMIDA_SECRET_CLIENT_ID");
-
-                    // code flow
-                    options.ResponseType = OpenIdConnectResponseType.Code;
-                    // выключаю спецом (хотя общая рекомендация с ним)
-                    options.UsePkce = false;
-                    
-                    options.Scope.Clear();
-                    options.Scope.Add("openid");
-                    options.Scope.Add("profile");
-                    options.Scope.Add("email");
-
-                    options.SaveTokens = true;
-                    options.UseTokenLifetime = true;
-                });
+                    ValidateAudience = false,
+                    NameClaimType = "name",
+                    RoleClaimType = "role"
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -70,9 +49,9 @@ namespace mvc_client
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
